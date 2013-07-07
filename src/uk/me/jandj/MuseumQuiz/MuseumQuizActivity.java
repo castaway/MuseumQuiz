b45fd62beb67190d38d3222fb14e4553eaf47144
Jess Robinson <castaway@desert-island.me.uk>
2013-07-07 12:11:37 +0100

    Display first (current) question correctly (and manually load a test question into the DB)

--- src/uk/me/jandj/MuseumQuiz/MuseumQuizActivity.java
+++ src/uk/me/jandj/MuseumQuiz/MuseumQuizActivity.java
@@ -3,6 +3,9 @@ package uk.me.jandj.MuseumQuiz;
 import android.app.Activity;
 import android.os.Bundle;
 import android.database.sqlite.*;
+import android.widget.*;
+import android.database.*;
+import java.util.*;
 
 public class MuseumQuizActivity extends Activity
 {
@@ -16,17 +19,85 @@ public class MuseumQuizActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		currentQuestion = savedInstanceState.getInt("currentQuestion", 1);
-		currentScore = savedInstanceState.getInt("currentScore", 0);
+		if(savedInstanceState != null) {
+			currentQuestion = savedInstanceState.getInt("currentQuestion", 1);
+			currentScore = savedInstanceState.getInt("currentScore", 0);
+		} else {
+			currentQuestion = 1;
+			currentScore = 0;
+		}
         setContentView(R.layout.main);
     }
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putInt("currentQuestion", currentQuestion);
+		outState.putInt("currentScore", currentScore);
+	}
 	@Override 
 	protected void onStart() {
 		super.onStart();
 		quizDb = new MuseumQuizDatabaseHelper(this);
 		displayNextQuestion(currentQuestion);		
 	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
 	
 	private void displayNextQuestion(int question) {
+		SQLiteDatabase readDb = quizDb.getReadableDatabase();
+//		System.err.println("Path = " + readDb.getPath());
+		String nextQSQL = "SELECT questions.desc, answers.id, answers.answer FROM quizzes " +
+		"JOIN questions ON quizzes.id = questions.quiz_id " +
+		"JOIN answers ON questions.id = answers.question_id " +
+		"WHERE quizzes.id = ? AND questions.id = ? " +
+		"ORDER BY answers.id";
+		String countQSQL = "SELECT COUNT(*) as count FROM questions WHERE quiz_id = ?";
+		
+		String[] countArgs = {String.valueOf(currentQuiz)};
+		Cursor countQCursor = readDb.rawQuery(countQSQL, countArgs);
+		int totalQuestions;
+		countQCursor.moveToFirst();
+		totalQuestions = countQCursor.getInt(countQCursor.getColumnIndex("count"));
+		countQCursor.close();
+		
+		TextView nOfM = (TextView)findViewById(R.id.n_of_m);
+		nOfM.setText("Question " + question + " of " + totalQuestions);
+		
+//		System.err.println("currentQuiz " + currentQuiz + " question " + question);
+		String[] nextQArgs = {String.valueOf(currentQuiz), String.valueOf(question)};
+		Cursor nextQCursor = readDb.rawQuery(nextQSQL, nextQArgs);
+		nextQCursor.moveToFirst();
+		String[] cols = nextQCursor.getColumnNames();
+		int rows = nextQCursor.getCount();
+		System.err.println("Rows " + rows);
+		
+		int descIndex = nextQCursor.getColumnIndex("desc");
+		System.err.println("index of desc col "+ descIndex);
+		String questionDesc = nextQCursor.getString(nextQCursor.getColumnIndex("desc"));
+		TextView questionView = (TextView)findViewById(R.id.question_text);
+		questionView.setText(questionDesc);
+
+		HashMap<Integer, Integer> buttonMap = new HashMap();
+		buttonMap.put(1, R.id.answer_1);
+		buttonMap.put(2, R.id.answer_2);
+		buttonMap.put(3, R.id.answer_3);
+		buttonMap.put(4, R.id.answer_4);
+		int countButton = 0;
+		do {
+			int answerId = Integer.parseInt(nextQCursor.getString(nextQCursor.getColumnIndex("id")));
+			String answerText = nextQCursor.getString(nextQCursor.getColumnIndex("answer"));
+			countButton++;
+			System.err.println("Answer row: id=" + answerId + " text=" + answerText + " btn="+countButton);
+			Button answerButton = (Button)findViewById(buttonMap.get(countButton));
+			answerButton.setText(answerText);
+			answerButton.setTag(new Integer(answerId));
+			
+		} while (nextQCursor.moveToNext());
+		nextQCursor.close();
 	}
 }
--- src/uk/me/jandj/MuseumQuiz/MuseumQuizDatabaseHelper.java
+++ src/uk/me/jandj/MuseumQuiz/MuseumQuizDatabaseHelper.java
@@ -11,11 +11,26 @@ public class MuseumQuizDatabaseHelper extends SQLiteOpenHelper
 	"id INTEGER PRIMARY KEY, " +
 	"name TEXT);";
 
+	private static final String QUESTION_TABLE_CREATE =
+	"CREATE TABLE questions (" +
+	"id INTEGER PRIMARY KEY, " +
+	"quiz_id INTEGER, " +
+	"desc TEXT);";
+
 	private static final String ANSWER_TABLE_CREATE =
 	"CREATE TABLE answers (" +
-	"quiz_id INTEGER, " +
+	"id INTEGER PRIMARY KEY, " +
+	"question_id INTEGER, " +
 	"answer TEXT, " + 
 	"correct BOOLEAN);";
+	
+	private static final String INITIAL_DATA =
+	"INSERT INTO quizzes VALUES (1, 'Robot Quiz');" +
+	"INSERT INTO questions VALUES (1, 1, 'What model was Arnold Swarznegger's Terminator?');" +
+	"INSERT INTO answers VALUES(1, 1, 'T-100', 0);" +
+	"INSERT INTO answers VALUES(2, 1, 'T-20', 0);" +
+	"INSERT INTO answers VALUES(3, 1, 'T-800', 1);" +
+	"INSERT INTO answers VALUES(4, 1, 'T-1000', 0);";
 	
 	MuseumQuizDatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,7 +39,42 @@ public class MuseumQuizDatabaseHelper extends SQLiteOpenHelper
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(QUIZ_TABLE_CREATE);
+		db.execSQL(QUESTION_TABLE_CREATE);
 		db.execSQL(ANSWER_TABLE_CREATE);
+//		db.execSQL(INITIAL_DATA);
+		ContentValues row = new ContentValues();
+		row.put("id", 1);
+		row.put("name", "Robots Quiz");
+		db.insert("quizzes", "id", row);
+		row = new ContentValues();
+		row.put("id", 1);
+		row.put("quiz_id", 1);
+		row.put("desc", "What model was Arnold Swarznegger's Terminator?");
+		db.insert("questions", "id", row);
+		row = new ContentValues();
+		row.put("id", 1);
+		row.put("question_id", 1);
+		row.put("answer", "T-100");
+		row.put("correct", 0);
+		db.insert("answers", "id", row);
+		row = new ContentValues();
+		row.put("id", 2);
+		row.put("question_id", 1);
+		row.put("answer", "T-20");
+		row.put("correct", 0);
+		db.insert("answers", "id", row);
+		row = new ContentValues();
+		row.put("id", 3);
+		row.put("question_id", 1);
+		row.put("answer", "T-800");
+		row.put("correct", 1);
+		db.insert("answers", "id", row);
+		row = new ContentValues();
+		row.put("id", 4);
+		row.put("question_id", 1);
+		row.put("answer", "T-1000");
+		row.put("correct", 0);
+		db.insert("answers", "id", row);
 	}
     
     @Override 
